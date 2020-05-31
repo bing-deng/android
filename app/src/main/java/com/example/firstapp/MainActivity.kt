b9e9030ea438593e0c02a4c649e229f53c636e5f
dengbb <804433988@qq.com>
2020-05-31 18:46:05 +0900

    数据持久化

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.firstapp
 
+import android.R.attr.data
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Environment
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.security.crypto.MasterKeys
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
 
 
 const val EXTRA_MESSAGE = "com.example.firstApp.MESSAGE"
@@ -18,9 +24,107 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        this.setSpf("123")
+        this.getSpf(R.string.user_pwd)
 
+        this.setShareSpf("456")
+        this.getShareSpf(R.string.user_pwd2)
+
+        this.setInnerSpace("doc","hello world\n how r u")
+        this.deleteInnerSpace("doc")
+        this.getInnerSpace("doc")
     }
 
+
+    fun setSpf( value:String){
+
+        val sharedPref = this?.getPreferences(Context.MODE_PRIVATE)
+        with (sharedPref.edit()) {
+            putString(getString(R.string.user_pwd), value)
+            commit()
+        }
+    }
+
+    fun getSpf(key:Int):String{
+
+        val sharedPref = this?.getPreferences(Context.MODE_PRIVATE)
+        var value = sharedPref.getString(getString(key), "defaultValue")
+        return value?:""
+    }
+
+
+    fun setShareSpf( value:String){
+
+        val sharedPref = this?.getSharedPreferences(getString(R.string.share_preference_01),Context.MODE_PRIVATE)
+        with (sharedPref.edit()) {
+            putString(getString(R.string.user_pwd2), value)
+            commit()
+        }
+    }
+
+    fun getShareSpf(key:Int):String{
+
+        val sharedPref = this?.getSharedPreferences(getString(R.string.share_preference_01),Context.MODE_PRIVATE)
+        var value = sharedPref.getString(getString(key), "defaultValue")
+        return value?:""
+    }
+
+
+    fun setInnerSpace(filename:String,fileContents:String){
+        // ioexception
+        val phonefreeSpace: Long = Environment.getDataDirectory().getFreeSpace()
+
+        this.openFileOutput(filename, Context.MODE_PRIVATE).use {
+            it.write(fileContents.toByteArray())
+        }
+    }
+
+    fun setInnerCacheSpace(filename:String,fileContents:String){
+        // ioexception
+        val phonefreeSpace: Long = Environment.getDataDirectory().getFreeSpace()
+
+        this.openFileOutput(filename, Context.MODE_PRIVATE).use {
+            it.write(fileContents.toByteArray())
+        }
+    }
+
+    fun deleteInnerSpace(filename:String){
+
+//        val file = File(this.filesDir, filename)
+//        val r = file.delete()
+//        println(r)
+        // 第二种删除方式
+        var r = this.deleteFile(filename)
+        println(r)
+
+    }
+    fun getInnerSpace(filename:String ){
+//        val file =   this.openFileInput(filename)
+//        println(file.toString())
+//        //把文件内容读取进缓冲读取器（use方法会自动对BufferedReader进行关闭）
+//        BufferedReader(InputStreamReader(file)).use {
+//            var line: String
+//            while (true) {
+//                line = it.readLine() ?: break //当有内容时读取一行数据，否则退出循环
+//                print(line)
+//            }
+//
+//        }
+
+        try {
+            val fileInput = openFileInput(filename)
+            fileInput.reader().forEachLine {
+                println(it)
+            }
+            fileInput.close()
+        }catch (e:Exception){
+            println(e)
+        }
+
+
+
+
+    }
     fun signIn(view:View){
 
         val i = Intent(this, SignActivity::class.java)
--- app/src/main/java/com/example/firstapp/MotionActivity.kt
+++ app/src/main/java/com/example/firstapp/MotionActivity.kt
@@ -1,5 +1,6 @@
 package com.example.firstapp
 
+import android.content.Context
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -11,6 +12,28 @@ class MotionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_motion)
 
-
+        this.getSpf(R.string.user_pwd)
+        this.getShareSpf(R.string.user_pwd2)
     }
+
+    /**
+     * 保存键值对数据
+     *
+     */
+
+
+    fun getSpf(key:Int):String{
+
+        val sharedPref = this?.getPreferences(Context.MODE_PRIVATE)
+        var value = sharedPref.getString(getString(key), "defaultValue")
+        return value?:""
+    }
+
+    fun getShareSpf(key:Int):String{
+
+        val sharedPref = this?.getSharedPreferences(getString(R.string.share_preference_01),Context.MODE_PRIVATE)
+        var value = sharedPref.getString(getString(key), "defaultValue")
+        return value?:""
+    }
+
 }
